7823107fce3f5c7a912d927db40d23a3ae305828
Hirofumi Omi <hirofumi.omi@3-shake.com>
2025-02-23 14:27:40 +0900

    asobi

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 mod handlers;
 
-use axum::{routing::get, Router};
-use handlers::hello::{echo, string_handler};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use handlers::{
+    hello::{echo, string_handler},
+    record::create_record,
+};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/echo", get(string_handler));
+        .route("/echo", get(string_handler))
+        .route("/records", post(create_record));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
